e6e1aa69e759f3d2fd7547deab3d3826588cbd0b
kartik7807 <mahorkartik007@gamil.com>
2025-09-16 21:17:23 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
-//const port=8080;
-const port = process.env.PORT || 3000; // ✅ Important for Render
+const port=8080;
+// const port = process.env.PORT || 3000; // ✅ Important for Render
 const path=require("path");
 const {v4: uuidv4}=require('uuid');
 const methodOverride = require('method-override')
